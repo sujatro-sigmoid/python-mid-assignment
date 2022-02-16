f206dac85b7977d3a25cca2cec523be3c64485c2
sujatro-sigmoid <98107412+sujatro-sigmoid@users.noreply.github.com>
2022-02-17 04:39:47 +0530

    Update test_assgn_unittests.py

--- test_assgn_unittests.py
+++ test_assgn_unittests.py
@@ -35,7 +35,17 @@ class MyTestCase(unittest.TestCase):
         self.assertRaises(python_assgn_exception_tdd.InvalidTypeError,
                           log.answer_stream, [[-9, 1.5], [2, "bar"], [3, "foo"]])
 
-    def test_case_6(self):  # timestamp not in order
+    def test_case_6(self):  # inputs violating constraints #1
+        log = python_assgn_exception_tdd.Logger()
+        self.assertRaises(python_assgn_exception_tdd.InputsViolatingConstraintsError,
+                          log.answer_stream, [[-1, "foo"], [2, "boo"], [3, "foo"]])
+
+    def test_case_7(self):  # inputs violating constraints #2
+        log = python_assgn_exception_tdd.Logger()
+        self.assertRaises(python_assgn_exception_tdd.InputsViolatingConstraintsError,
+                          log.answer_stream, [[1, "foo"], [2, "aaaaaaaaaabbbbbbbbbbccccccccccd"], [3, "foo"]])
+
+    def test_case_8(self):  # timestamp not in order
         log = python_assgn_exception_tdd.Logger()
         self.assertRaises(python_assgn_exception_tdd.TimestampsNotInOrderError,
                           log.answer_stream, [[1, "foo"], [5, "boo"], [3, "foo"]])
@@ -43,3 +53,5 @@ class MyTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+    
